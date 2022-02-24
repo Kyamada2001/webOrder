01ddf110a8d92bb1ac0d6db229954c133b7f08cf
Kyamada2001 <yamakai173git@gmail.com>
2022-02-25 01:50:01 +0900

    コミット漏れ

--- server/webOrder/routes/web.php
+++ server/webOrder/routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 //管理側
-Route::get('/business/shops', 'Business\ShopController@index')->name('shop.index');
-Route::get('business/shops/create', 'Business\ShopController@create')->name('shop.create');
+Route::get('/business/shops', 'Business\ShopController@index')->name('business.shop.index');
+Route::get('business/shops/create', 'Business\ShopController@create')->name('business.shop.create');
 
-
+Route::post('/business/shop/store', 'Business\ShopController@store')->name('business.shop.store');
 
 
 //お客さん側
