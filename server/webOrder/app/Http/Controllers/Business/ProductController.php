c84a4e654811c9b0a2099cc3e3346a79edca617c
Kyamada2001 <yamakai173git@gmail.com>
2022-02-26 00:58:25 +0900

    商品一覧画面を表示

--- server/webOrder/app/Http/Controllers/Business/ProductController.php
+++ server/webOrder/app/Http/Controllers/Business/ProductController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers\Business;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Shop;
 
 class ProductController extends Controller
 {
     public function index(){
-        return view('business.products.index');
+        $products = Product::with('shop')->get();
+        return view('business.products.index',[
+            'products' => $products,
+        ]);
     }
 }
--- server/webOrder/resources/views/business/products/index.blade.php
+++ server/webOrder/resources/views/business/products/index.blade.php
@@ -1,7 +1,38 @@
 @extends('business.layouts.app')
 @section('title', '商品一覧')
 @section('content')
-<div>
-    <input type="text"/>
+<div class="p-6 bg-white">
+    <div class="flex justify-center">
+        <a class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 border rounded" href="#">
+            新規登録
+        </a>
+    </div>
+    <div class="p-6 bg-whitez">
+        <div>
+            <p>全{{ count($products) }}件</p>
+        </div>
+        <div>
+            <table class="min-w-full">
+                <thead class="bg-gray-50 dark:bg-gray-700">
+                    <tr>
+                        <td class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">商品ID</td>
+                        <td class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">販売店舗</td>
+                        <td class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">商品名</td>
+                        <td class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">商品金額</td>
+                    </tr>
+                </thead>
+                @foreach($products as $product)
+                <tbody>
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $product->id }}</td>
+                        <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $product->shop->name }}</td>
+                        <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $product->name }}</td>
+                        <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $product->price }}円</td>
+                    </tr>
+                </tbody>
+                @endforeach
+            </table>
+        </div>
+    </div>
 </div>
 @endsection
