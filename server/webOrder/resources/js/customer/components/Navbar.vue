16d542744eadf16a13b668f6221117f0604cde28
Kyamada2001 <yamakai173git@gmail.com>
2022-03-08 08:26:26 +0900

    customer側のヘッダー項目追加。

--- server/webOrder/resources/js/customer/components/Navbar.vue
+++ server/webOrder/resources/js/customer/components/Navbar.vue
@@ -5,11 +5,16 @@
             <h1 class="px-10 py-1 text-sm font-serif text-gray-500">気軽にwebで注文できるサービス</h1>
         </div>
     </div>
-    <div class="flex flex-row justify-items-stretch">
-        <div class="px-10 pb-3 w-screen">
+    <div class="flex flex-row justify-around w-full">
+        <div class="px-10 pb-3 w-1/5">
             <h1 class="font-serif text-2xl text-orange-500">webOrder</h1>
         </div>
-        <div class="flex flex-row inset-y-0 right-0 px-4 pb-5 justify-end items-end w-screen">
+        <div class="w-3/5 space-x-5">
+            <router-link class="hover:text-orange-300 text-lg" to="#">店舗一覧</router-link>
+            <router-link class="hover:text-orange-300 text-lg" to="#">商品一覧</router-link>
+            <router-link class="hover:text-orange-300 text-lg" to="#">店舗一覧</router-link>
+        </div>
+        <div class="flex flex-row inset-y-0 right-0 px-4 pb-5 justify-end items-end w-1/3">
             <div class="px-4">
                 <router-link class="hover:text-orange-300 text-sm" to="#">会員登録</router-link>
             </div>
