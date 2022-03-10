67580163250d7ba6bdf9e5d4def7a655db3131f5
Kyamada2001 <yamakai173git@gmail.com>
2022-03-10 23:32:37 +0900

    トップのvue-routerのルーティングを作成

--- server/webOrder/resources/js/customer/router.js
+++ server/webOrder/resources/js/customer/router.js
@@ -1,13 +1,19 @@
 import Router from 'vue-router'
 import MemberRegister from './views/member/Register.vue'
+import Top from './views/Top.vue'
 
 export default new Router({
   mode: 'history',
   routes: [
     {
+      path: '/',
+      name: 'top',
+      component: Top
+    },
+    {
       path: '/member/register',
       name: 'member-register',
       component: MemberRegister
-    }
+    },
   ]
 });
---
+++ server/webOrder/resources/js/customer/views/Top.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex justify-center text-2xl font-semibold">
+        トップ画面
+    </div>
+</template>
