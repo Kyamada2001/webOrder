4e8e08f6f617d24df6c58070f1be72265c219519
Kyamada2001 <yamakai173git@gmail.com>
2022-03-16 20:32:14 +0900

    コメント追加

--- server/webOrder/app/Models/Customer.php
+++ server/webOrder/app/Models/Customer.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable; 
 
-class Customer extends Authenticatable
+class Customer extends Authenticatable //ログインの判別をするテーブルのモデルはAuthenticatableを継承する
 {
     use HasFactory;
 
