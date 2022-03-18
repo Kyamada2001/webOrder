f5a90e458fd7359db243dee6a9206aac470982a8
Kyamada2001 <yamakai173git@gmail.com>
2022-03-19 01:04:15 +0900

    vuexのstoreを使用して会員登録可能に。会員登録完了画面に遷移まで

--- server/webOrder/resources/js/customer/store/auth.js
+++ server/webOrder/resources/js/customer/store/auth.js
@@ -1,10 +1,31 @@
-const state = {}
+const state = {
+    customer: null
+}
 
-const getters = {}
+const getters = {
+    check: state => !! state.customer,
+    customername: state => state.customer ? state.customer.username : '',
+}
 
-const mutations = {}
+const mutations = {
+    setCustomer(state, customer){
+        state.customer = customer
+    }
+}
 
-const actions = {}
+const actions = {
+    async register(context, data){
+        const response = await axios.post('/api/register' ,{
+            //register: this.registerForm view側でまとめて送りたい
+            username: data.username,
+            email: data.email,
+            password: data.password,
+            password_confirmation: data.password_confirmation,
+        });
+        context.commit('setCustomer', response.data);
+        console.log(response.data);
+    }
+}
 
 export default {
   namespaced: true,
--- server/webOrder/resources/js/customer/views/member/Register-complete.vue
+++ server/webOrder/resources/js/customer/views/member/Register-complete.vue
@@ -1,5 +1,21 @@
 <template>
 	<div class="flex flex-col items-center space-y-5">
         <div class="text-2xl pt-8 font-semibold">会員完了</div>
+		<div>
+			<div>{{ this.login_customer }}さん！会員登録ありがとうございます！</div>
+		</div>
 	</div>
 </template>
+<script>
+export default{
+	data(){
+		return {
+			login_customer: '',
+		}
+	},
+	created: function(){
+		this.login_customer = this.$store.getters['auth/customername'];
+		console.log(this.$store.getters['auth/customername']);
+	}
+}
+</script>
