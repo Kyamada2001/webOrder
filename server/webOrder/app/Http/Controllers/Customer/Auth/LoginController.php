40771e0013b67bae15d993dbd02f5f623e8ad17a
Kyamada2001 <yamakai173git@gmail.com>
2022-03-24 23:25:22 +0900

    ログイン失敗時に401を返すように

--- server/webOrder/app/Http/Controllers/Customer/Auth/LoginController.php
+++ server/webOrder/app/Http/Controllers/Customer/Auth/LoginController.php
@@ -7,6 +7,7 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request; 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\JsonResponse;
 
 class LoginController extends Controller
 {
@@ -40,18 +41,13 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    protected function authenticated(Request $request, $user)
-    {
-        return $user;
-    }
-
     public function authenticate(Request $request)
     {
-        Auth::guard('customer');
         $authentication_info = $request->only('email','password');
-
-        if (Auth::guard('customer')->attempt($authentication_info)) {
+        if(Auth::guard('customer')->attempt($authentication_info)){
             return Auth::guard('customer')->user();
+        }else{
+            return response()->json([],401);
         }
     }
 
