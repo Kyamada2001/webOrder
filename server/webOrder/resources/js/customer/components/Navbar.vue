4c3fb9cbcc6c0092eb2449c75c50b433eeedd0f7
Kyamada2001 <yamakai173git@gmail.com>
2022-03-26 16:53:02 +0900

    ヘッダーのレイアウト修正

--- server/webOrder/resources/js/customer/components/Navbar.vue
+++ server/webOrder/resources/js/customer/components/Navbar.vue
@@ -9,12 +9,12 @@
         <div class="px-10 pb-3 w-1/5">
             <router-link class="font-serif text-2xl text-orange-500" to="/">webOrder</router-link>
         </div>
-        <div class="flex justify-center w-3/5 space-x-10">
+        <div class="flex justify-center w-full space-x-10">
             <router-link class="hover:text-orange-300 text-lg" to="#">店舗一覧</router-link>
             <router-link class="hover:text-orange-300 text-lg" to="#">商品一覧</router-link>
             <router-link class="hover:text-orange-300 text-lg" to="#">店舗一覧</router-link>
         </div>
-        <div v-if="isLogin" class="flex flex-row inset-y-0 right-0 px-4 pb-5 justify-end items-end w-1/3">
+        <div v-if="isLogin" class="flex flex-row inset-y-0 right-0 px-4 pb-5 justify-end items-end w-full">
             <div class="px-4">
                 <router-link to="#" class="hover:text-orange-300 text-sm" >{{ login_customer }}</router-link>
             </div>
@@ -32,7 +32,7 @@
                 </logout-modal>
             </div>
         </div>
-        <div v-else class="flex flex-row inset-y-0 right-0 px-4 pb-5 justify-end items-end w-1/3">
+        <div v-else class="flex flex-row inset-y-0 right-0 px-4 pb-5 justify-end items-end w-full">
             <div class="px-4">
                 <router-link class="hover:text-orange-300 text-sm" to="/member/select-register">会員登録</router-link>
             </div>
