9dbf70db1acec975e6c932bf2c00a6b90e4bd590
Kyamada2001 <yamakai173git@gmail.com>
2022-11-08 06:04:04 +0900

    [レスポンシブデザイン]ボトムナビゲーションにカートを追加

--- server/webOrder/resources/js/customer/Home.vue
+++ server/webOrder/resources/js/customer/Home.vue
@@ -1,15 +1,15 @@
 <template>
-    <diV class="h-fit">
+    <diV class="h-full">
         <header class="bg-stone-100 h-20 border-t-4 border-b-2 border-t-orange-400 border-b-gray-300 shadow-sm sticky top-0">
             <Navbar/>
         </header>
 
-        <main>
+        <main class="h-full">
             <router-view/>
         </main>
 
         <!-- 画面縮小時に表示 -->
-        <footer class="md:hidden bg-stone-100 h-20 border border-b-gray-300 shadow-sm sticky bottom-0">
+        <footer class="md:hidden bg-stone-100 h-20 border border-b-gray-300 shadow-sm sticky bottom-0 mt-2">
             <BottomNavigation/>
         </footer>
     </diV>
