9c861c08a47e7a5b5ac79fd5c83540b86fc8d58f
Kyamada2001 <yamakai173git@gmail.com>
2022-12-30 17:50:19 +0900

    [管理画面]テーブルUI変更

--- server/webOrder/resources/views/business/products/index.blade.php
+++ server/webOrder/resources/views/business/products/index.blade.php
@@ -1,43 +1,43 @@
 @extends('business.layouts.app')
 @section('title', '商品一覧')
 @section('content')
-<div id="app" class="p-6 bg-white">
+<div id="app" class="p-1 md:p-6 bg-white">
     <div class="flex justify-center">
         <a class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 border rounded" href="{{ route('business.product.create') }}">
             新規登録
         </a>
     </div>
-    <div class="p-6 bg-whitez">
+    <div class="p-1 md:p-6 bg-white">
         <div>
             <p>全{{ count($products) }}件</p>
         </div>
-        <div>
-            <table class="min-w-full">
+        <div class="box-border">
+            <table class="w-full">
                 <thead class="bg-gray-50 dark:bg-gray-700">
                     <tr>
-                        <td class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">商品ID</td>
-                        <td class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">商品名</td>
-                        <td class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">商品画像</td>
-                        <td class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">販売店舗</td>
-                        <td class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">商品金額</td>
-                        <td class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">おすすめ</td>
+                        <td class="py-3 px-1 md:px-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">商品ID</td>
+                        <td class="py-3 px-1 md:px-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">商品名</td>
+                        <td class="py-3 px-1 md:px-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">商品画像</td>
+                        <td class="py-3 px-1 md:px-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">販売店舗</td>
+                        <td class="py-3 px-1 md:px-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">商品金額</td>
+                        <td class="py-3 px-1 md:px-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">おすすめ</td>
                     </tr>
                 </thead>
                 @foreach($products as $product)
                 <tbody>
                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $product->id }}</td>
-                        <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-sky-400 hover:underline decoration-solid"><a href="{{ route('business.product.edit', ['product' => $product]) }}">{{ $product->name }}</a></td>
-                        <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <td class="py-4 px-1 md:px-3 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $product->id }}</td>
+                        <td class="py-4 px-1 md:px-3 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-sky-400 hover:underline decoration-solid"><a href="{{ route('business.product.edit', ['product' => $product]) }}">{{ $product->name }}</a></td>
+                        <td class="py-4 px-1 md:px-3 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             @if(!empty($product->imgpath))
                             <img class="h-24 w-28 object-cover" src="{{ asset('storage/' . $product->imgpath) }}">
                             @else
                             <img class="h-24 w-28 object-cover" src="{{ asset('storage/images/noimage.png') }}">
                             @endif
                         </td>
-                        <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $product->shop->name }}</td>
-                        <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $product->price }}円</td>
-                        <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <td class="py-4 px-1 md:px-3 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $product->shop->name }}</td>
+                        <td class="py-4 px-1 md:px-3 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $product->price }}円</td>
+                        <td class="py-4 px-1 md:px-3 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             @if($product->recommendation_flg == 1)
                             <label class="text-sm bg-orange-400 text-white font-bold py-1 px-2 rounded-full text-center">おすすめ</label>
                             @else
--- server/webOrder/resources/views/business/shops/index.blade.php
+++ server/webOrder/resources/views/business/shops/index.blade.php
@@ -1,13 +1,13 @@
 @extends('business.layouts.app')
 @section('title','店舗一覧')
 @section('content')
-<div id="app" class="p-6 bg-white">
+<div id="app" class="p-1 md:p-6 bg-white">
     <div class="flex justify-center">
         <a class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 border rounded" href="{{ route('business.shop.create') }}">
             新規登録
         </a>
     </div>
-    <div class="p-6 bg-whitez">
+    <div class="p-1 md:p-6 bg-white">
         <div class="pb-2">
             <p>全{{ count($shops) }}件</p>
         </div>
@@ -15,29 +15,29 @@
             <table class="min-w-full">
                 <thead class="bg-gray-50 dark:bg-gray-700">
                     <tr>
-                        <td class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">店舗ID</td>
-                        <td class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">店舗名</td>
-                        <td class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">店舗画像</td>
-                        <td class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">営業時間(開店時間)</td>
-                        <td class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">営業時間(閉店時間)</td>
-                        <td class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400 w-36">週休</td>
+                        <td class="py-3 px-1 md:px-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">店舗ID</td>
+                        <td class="py-3 px-1 md:px-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">店舗名</td>
+                        <td class="py-3 px-1 md:px-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">店舗画像</td>
+                        <td class="py-3 px-1 md:px-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">営業時間(開店時間)</td>
+                        <td class="py-3 px-1 md:px-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">営業時間(閉店時間)</td>
+                        <td class="py-3 px-1 md:px-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400 w-36">週休</td>
                     </tr>
                 </thead>
                 @foreach($shops as $shop)
                 <tbody>
                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $shop->id }}</td>
-                        <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-sky-400 hover:underline decoration-solid"><a href="{{ route('business.shop.edit', ['shop' => $shop]) }}">{{ $shop->name }}</a></td>
-                        <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <td class="py-3 px-1 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $shop->id }}</td>
+                        <td class="py-3 px-1 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-sky-400 hover:underline decoration-solid"><a href="{{ route('business.shop.edit', ['shop' => $shop]) }}">{{ $shop->name }}</a></td>
+                        <td class="py-3 px-1 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             @isset($shop->imgpath)
                                 <img class="h-24 w-28 object-cover" src="{{ asset('storage/' . $shop->imgpath) }}">
                             @else
                                 <img class="h-24 w-28 object-cover" src="{{ asset('storage/' . 'images/noimage.png') }}">
                             @endisset
                         </td>
-                        <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $shop->business_start_time }}</td>
-                        <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $shop->business_end_time }}</td>
-                        <td class="py-4 px-6 text-xs font-medium text-gray-900 whitespace-nowrap dark:text-white w-36">
+                        <td class="py-3 px-1 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $shop->business_start_time }}</td>
+                        <td class="py-3 px-1 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $shop->business_end_time }}</td>
+                        <td class="py-3 px-1 text-xs font-medium text-gray-900 whitespace-nowrap dark:text-white w-36">
                             <div class="flex flex-wrap">
                             @foreach(str_split($shop->weekly_holiday) as $holiday)
                                 @switch($holiday)
