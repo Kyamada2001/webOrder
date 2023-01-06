ea171b95f801e311c5ae76cda516024d07efba97
Kyamada2001 <yamakai173git@gmail.com>
2023-01-07 01:17:49 +0900

    微修正

--- server/webOrder/resources/views/business/customers/index.blade.php
+++ server/webOrder/resources/views/business/customers/index.blade.php
@@ -13,16 +13,14 @@
                         <td class="thead-item">ID</td>
                         <td class="thead-item">ユーザー名</td>
                         <td class="thead-item">メールアドレス</td>
-                        <td class="thead-item">アカウント作成日時</td>
                     </tr>
                 </thead>
                 @foreach($customers as $customer)
                 <tbody>
                     <tr class="bg-white bcustomer-b dark:bg-gray-800 dark:bcustomer-gray-700">
-                        <td class="py-3 px-1 text-sm font-medium text-sky-400 whitespace-nowrap dark:text-sky-400 hover:underline decoration-solid">{{ $customer->id }}</td>
+                        <td class="py-3 px-1 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $customer->id }}</td>
                         <td class="py-3 px-1 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $customer->username }}</td>
                         <td class="py-3 px-1 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $customer->email }}</td>
-                        <td class="py-3 px-1 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $customer->created_at }}</td>
                     </tr>
                 </tbody>
                 @endforeach
