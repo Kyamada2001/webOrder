961c1a178284cf143b6cb2425f720155d4ce6353
Kyamada2001 <yamakai173git@gmail.com>
2023-01-15 00:54:24 +0900

    [ユーザー側]受取日時表レイアウト修正

--- server/webOrder/resources/js/customer/views/order/cart-confirmation.vue
+++ server/webOrder/resources/js/customer/views/order/cart-confirmation.vue
@@ -32,8 +32,8 @@
                         <div class="flex overflow-x-auto">
                             <div class="flex flex-row w-full flex-none">
                                 <div class="flex flex-row w-full">
-                                    <div class="border border-gray-300 order-date-lists" v-for="dateTime in dateTimes" :key="dateTime.id">
-                                        <div class="justify-content-center border-b border-gray-300 px-2 py-1">{{ dateTime.month }}/{{ dateTime.date }}</div>
+                                    <div class="order-date-lists" v-for="dateTime in dateTimes" :key="dateTime.id">
+                                        <div class="justify-content-center border-b border-gray-300 px-5 py-1">{{ dateTime.month }}/{{ dateTime.date }}</div>
 
                                         <div v-if="dateTime.orderAbleFlg === 0" class="bg-gray-200 flex flex-col h-12">
                                             <div class="mx-auto">{{ displayDayOfWeek(dateTime.dayOfWeek) }}</div>
@@ -44,7 +44,7 @@
                                         <div v-else @click="openOrderTimeModal(dateTime)" class="cursor-pointer bg-orange-100 hover:bg-orange-50 h-12 flex flex-col">
                                             <div class="mx-auto">{{ displayDayOfWeek(dateTime.dayOfWeek) }}</div>
                                             <div class="mx-auto">
-                                                <div v-if="storeOrderTime.joinDate && storeOrderTime.date == dateTime.date" class="text-sm bg-green-300 reounded align-bottom inline-block w-full mx-0">{{ storeOrderTime.time }}</div>
+                                                <div v-if="storeOrderTime.joinDate && storeOrderTime.date == dateTime.date" class=" bg-green-300 reounded align-bottom inline-block w-full px-2">{{ storeOrderTime.time }}</div>
                                                 <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 block">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59" />
                                                 </svg>
